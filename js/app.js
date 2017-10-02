300618891eb3db52daf6f373751ff277304cb65e
Konrad Pardyak <konradpardyak@gmail.com>
2017-10-02 12:06:36 +0200

    add playlist

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(()=>{
       this.video = document.getElementById('player-video');
       this.playButton = $('#play-button');
       this.stopButton = $('#stop-button');
-      this.progressBar = $('#player-progress');
+      this.progressBar = $('.progress-bar');
       this.volumeButton = $('#player-volume');
       this.playerInfo = $('#player-info');
       this.infoTimeout = false; //useful in showInfo method
@@ -14,24 +14,30 @@ $(()=>{
     preventControls(){
       this.video.controls = false;
     }
-
+    //change play and pause icon
     playAndPause(){
       if (this.video.paused || this.video.ended) {
         this.playButton.attr("title", "pause");
-        this.playButton.text("Pause");
+        this.playButton.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>');
         this.showInfo("Playing");
         this.video.play();
       }
       else {
         this.playButton.attr("title", "play");
-        this.playButton.text("Play");
+        this.playButton.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>');
         this.showInfo("Paused");
         this.video.pause();
       }
     }
 
     stop(){
-
+      this.playButton.attr("title", "play");
+      this.playButton.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>');
+      this.showInfo("Paused");
+      this.video.pause();
+      this.video.removeAttribute("autoplay");
+      this.video.removeAttribute("src");
+      this.video.load();
     }
 
     showProgress(){
@@ -39,12 +45,12 @@ $(()=>{
       if (this.video.currentTime > 0){
         progress = (100/this.video.duration)*this.video.currentTime;
       }
-      console.log(progress.toFixed());
+      this.progressBar.css("width", progress + "%");
     }
 
     changeVolume(){
       this.video.volume = this.volumeButton.val();
-      this.showInfo("Volume set to: " + (this.video.volume*100).toFixed(0) + "%");
+      this.showInfo("Volume: " + (this.video.volume*100).toFixed(0) + "%");
     }
 
     showInfo(text){
@@ -62,22 +68,102 @@ $(()=>{
       }, 1500);
 
     }
+
+    play(file){
+      this.video.setAttribute("autoplay", "");
+      this.video.src = file;
+      this.playAndPause();
+    }
   }
 
-  const player = new Player;
+  class Playlist{
+    constructor(){
+      this.list = [
+                    {href: 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
+                    img: 'http://camendesign.com/code/video_for_everybody/poster.jpg',
+                    alt: 'big-buck-bunny'},
+                    {href: 'http://thenewcode.com/assets/videos/atlantic-light.mp4',
+                    img: 'http://thenewcode.com/assets/images/trotternish-ridge-isle-of-skye.jpg',
+                    alt: 'atlantic-light'},
+                    {href: 'http://thenewcode.com/assets/videos/mountain.mp4',
+                    img: 'http://thenewcode.com/assets/images/vid-mountain.jpg',
+                    alt: 'mountain'},
+                    {href: 'http://thenewcode.com/assets/videos/ocean-diver.mp4',
+                    img: 'http://thenewcode.com/assets/images/ocean-diver.jpg',
+                    alt: 'ocean-diver'}
+                  ]
+      this.links = null;
+      this.currentVideo = 0;
+      this.nextVideo = this.list[1].href;
+    }
 
+    loadFiles(){
+      const $video = $('#player-video');
+      let $source = $('<source>').attr("id", "source").attr("src", this.list[0].href).attr("type", "video/mp4");
+      $video.append($source);
+
+      const $playlistUl = $('#playlist');
+      this.list.forEach((element, index)=>{
+        const $li = $('<li>');
+        const $a = $('<a>').attr("href", element.href).attr("data-number", index);
+        const $img = $('<img>').attr("src", element.img).attr("alt", element.alt);
+
+        $a.append($img);
+        $li.append($a);
+        $playlistUl.append($li);
+      });
+
+      this.links = $('#playlist a');
+    }
+
+    setNextVideo(){
+      if(this.currentVideo < this.list.length - 1){
+        this.nextVideo = this.list[parseInt(this.currentVideo) + 1].href;
+      } else{
+        this.nextVideo = this.list[1].href;
+      }
+    }
+
+  }
+
+  const playlist = new Playlist();
+  const player = new Player();
+
+  playlist.loadFiles();
   player.preventControls();
 
   player.playButton.on("click", ()=>{
     player.playAndPause();
   });
 
+  player.stopButton.on("click", ()=>{
+    player.stop();
+  });
+
   player.video.addEventListener("timeupdate", (e)=>{
     player.showProgress();
   }, false);
 
+  player.video.addEventListener("ended", ()=>{
+    if(playlist.currentVideo < playlist.list.length - 1){
+      player.play(playlist.nextVideo);
+      playlist.currentVideo ++;
+    } else{
+      player.stop();
+      playlist.currentVideo = 0;
+    }
+    playlist.setNextVideo();
+  });
+
   player.volumeButton.on("input", ()=>{
     player.changeVolume();
+  });
+
+  playlist.links.on("click", function(event){
+    event.preventDefault();
+    player.play(this.getAttribute("href"));
+    playlist.currentVideo = this.dataset["number"];
+    playlist.setNextVideo();
   });
 
 });
--- js/out.js
+++ js/out.js
@@ -82,7 +82,7 @@ $(function () {
       this.video = document.getElementById('player-video');
       this.playButton = $('#play-button');
       this.stopButton = $('#stop-button');
-      this.progressBar = $('#player-progress');
+      this.progressBar = $('.progress-bar');
       this.volumeButton = $('#player-volume');
       this.playerInfo = $('#player-info');
       this.infoTimeout = false; //useful in showInfo method
@@ -95,24 +95,34 @@ $(function () {
       value: function preventControls() {
         this.video.controls = false;
       }
+      //change play and pause icon
+
     }, {
       key: 'playAndPause',
       value: function playAndPause() {
         if (this.video.paused || this.video.ended) {
           this.playButton.attr("title", "pause");
-          this.playButton.text("Pause");
+          this.playButton.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>');
           this.showInfo("Playing");
           this.video.play();
         } else {
           this.playButton.attr("title", "play");
-          this.playButton.text("Play");
+          this.playButton.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>');
           this.showInfo("Paused");
           this.video.pause();
         }
       }
     }, {
       key: 'stop',
-      value: function stop() {}
+      value: function stop() {
+        this.playButton.attr("title", "play");
+        this.playButton.html('<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M8 5v14l11-7z"/></svg>');
+        this.showInfo("Paused");
+        this.video.pause();
+        this.video.removeAttribute("autoplay");
+        this.video.removeAttribute("src");
+        this.video.load();
+      }
     }, {
       key: 'showProgress',
       value: function showProgress() {
@@ -120,13 +130,13 @@ $(function () {
         if (this.video.currentTime > 0) {
           progress = 100 / this.video.duration * this.video.currentTime;
         }
-        console.log(progress.toFixed());
+        this.progressBar.css("width", progress + "%");
       }
     }, {
       key: 'changeVolume',
       value: function changeVolume() {
         this.video.volume = this.volumeButton.val();
-        this.showInfo("Volume set to: " + (this.video.volume * 100).toFixed(0) + "%");
+        this.showInfo("Volume: " + (this.video.volume * 100).toFixed(0) + "%");
       }
     }, {
       key: 'showInfo',
@@ -146,25 +156,108 @@ $(function () {
           _this.playerInfo.fadeOut();
         }, 1500);
       }
+    }, {
+      key: 'play',
+      value: function play(file) {
+        this.video.setAttribute("autoplay", "");
+        this.video.src = file;
+        this.playAndPause();
+      }
     }]);
 
     return Player;
   }();
 
+  var Playlist = function () {
+    function Playlist() {
+      _classCallCheck(this, Playlist);
+
+      this.list = [{ href: 'http://clips.vorwaerts-gmbh.de/VfE_html5.mp4',
+        img: 'http://camendesign.com/code/video_for_everybody/poster.jpg',
+        alt: 'big-buck-bunny' }, { href: 'http://thenewcode.com/assets/videos/atlantic-light.mp4',
+        img: 'http://thenewcode.com/assets/images/trotternish-ridge-isle-of-skye.jpg',
+        alt: 'atlantic-light' }, { href: 'http://thenewcode.com/assets/videos/mountain.mp4',
+        img: 'http://thenewcode.com/assets/images/vid-mountain.jpg',
+        alt: 'mountain' }, { href: 'http://thenewcode.com/assets/videos/ocean-diver.mp4',
+        img: 'http://thenewcode.com/assets/images/ocean-diver.jpg',
+        alt: 'ocean-diver' }];
+      this.links = null;
+      this.currentVideo = 0;
+      this.nextVideo = this.list[1].href;
+    }
+
+    _createClass(Playlist, [{
+      key: 'loadFiles',
+      value: function loadFiles() {
+        var $video = $('#player-video');
+        var $source = $('<source>').attr("id", "source").attr("src", this.list[0].href).attr("type", "video/mp4");
+        $video.append($source);
+
+        var $playlistUl = $('#playlist');
+        this.list.forEach(function (element, index) {
+          var $li = $('<li>');
+          var $a = $('<a>').attr("href", element.href).attr("data-number", index);
+          var $img = $('<img>').attr("src", element.img).attr("alt", element.alt);
+
+          $a.append($img);
+          $li.append($a);
+          $playlistUl.append($li);
+        });
+
+        this.links = $('#playlist a');
+      }
+    }, {
+      key: 'setNextVideo',
+      value: function setNextVideo() {
+        if (this.currentVideo < this.list.length - 1) {
+          this.nextVideo = this.list[parseInt(this.currentVideo) + 1].href;
+        } else {
+          this.nextVideo = this.list[1].href;
+        }
+      }
+    }]);
+
+    return Playlist;
+  }();
+
+  var playlist = new Playlist();
   var player = new Player();
 
+  playlist.loadFiles();
   player.preventControls();
 
   player.playButton.on("click", function () {
     player.playAndPause();
   });
 
+  player.stopButton.on("click", function () {
+    player.stop();
+  });
+
   player.video.addEventListener("timeupdate", function (e) {
     player.showProgress();
   }, false);
 
+  player.video.addEventListener("ended", function () {
+    if (playlist.currentVideo < playlist.list.length - 1) {
+      player.play(playlist.nextVideo);
+      playlist.currentVideo++;
+    } else {
+      player.stop();
+      playlist.currentVideo = 0;
+    }
+    playlist.setNextVideo();
+  });
+
   player.volumeButton.on("input", function () {
     player.changeVolume();
+  });
+
+  playlist.links.on("click", function (event) {
+    event.preventDefault();
+    player.play(this.getAttribute("href"));
+    playlist.currentVideo = this.dataset["number"];
+    playlist.setNextVideo();
   });
 });
 
